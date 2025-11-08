d6e556a2eedfbc02806607000c83ffa8e80d9cf8
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-11-08 23:29:04 +0530

    refactor: Move questions data from quiz.js to questions.js
    Moved the questions data from quiz.js to questions.js for cleaner separation of logic and data

---
+++ questions.js
@@ -0,0 +1,33 @@
+const quizData = [
+  {
+    question: "1) Which is the largest continent in the world?",
+    options: ["Asia", "North America", "Europe", "Antarctica"],
+    answer: "a",
+  },
+  {
+    question: `2) Which country is known as the "Land of the Rising Sun"?`,
+    options: ["China", "South Korea", "India", "Japan"],
+    answer: "d",
+  },
+  {
+    question: "3) What is the capital city of Canada?",
+    options: ["Vancouver", "Ottawa", "Calgary", "Toronto"],
+    answer: "b",
+  },
+  {
+    question:
+      "4) Which planet has the most moons in our solar system (as of 2025)?",
+    options: ["Saturn", "Jupiter", "Earth", "Mercury"],
+    answer: "a",
+  },
+  {
+    question: "5) Who was the first person to reach the South Pole?",
+    options: [
+      "Helmer Hanssen",
+      "Oscar Wisting",
+      "Roald Amundsen",
+      "Sverre Hassel",
+    ],
+    answer: "c",
+  },
+];
